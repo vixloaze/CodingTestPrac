9270ab55bcc43630f5727cdf251f393d26e6b79f
조성현 <agllwy116@naver.com>
2023-09-22 16:01:42 +0900

    백준 14916 : 거스름돈

---
+++ src/BaekJoonPython/Sliver5/question14916.py
@@ -0,0 +1,17 @@
+num = int(input())
+
+cnt = 0
+
+while True:
+    if num % 5 == 0:
+        cnt += num // 5
+        break
+    else:
+        num -= 2
+        cnt += 1
+    
+    if num <0:
+        break
+
+if num<0: print(-1)
+else: print(cnt)
