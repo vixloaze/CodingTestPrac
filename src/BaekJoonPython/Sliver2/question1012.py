1e9ec5c6ef10d2f1d43e29ea283b51e7587dc9db
조성현 <agllwy116@naver.com>
2023-11-02 10:41:17 +0900

    백준 1012 : 유기농 배추

--- src/BaekJoonPython/Sliver2/question1012.py
+++ src/BaekJoonPython/Sliver2/question1012.py
@@ -1,11 +1,32 @@
 import sys
-from collections import deque
+sys.setrecursionlimit(10000)
 
-input = sys.stdin.readline
+def dfs(x,y):
+    dx = [1, -1, 0 , 0]
+    dy = [0, 0, 1, -1]
+    
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        
+        if (0 <= nx < N) and (0 <= ny < M):
+            if matrix[nx][ny] == 1:
+                matrix[nx][ny] = -1
+                dfs(nx, ny)
 
 testcase = int(input())
-m,n,k = map(int, input().split())
-
-graph = []
-for i in range(m):
-    graph.append(list(map(int,input())))
+for _ in range(testcase):
+    M, N, K = map(int, input().split())
+    matrix = [[0]*M for _ in range(N)]
+    cnt =0 
+    
+    for _ in range(K):
+        m, n = map(int, input().split())
+        matrix[n][m] = 1
+    
+    for i in range(N):
+        for j in range(M):
+            if matrix[i][j] >0:
+                dfs(i, j)
+                cnt += 1 
+    print(cnt)
