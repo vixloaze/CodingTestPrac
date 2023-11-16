e75ccdb33321f189b32f04078ca701f97899c8dc
조성현 <agllwy116@naver.com>
2023-11-16 17:19:56 +0900

    백준 9465 : 스티커

---
+++ src/BaekJoonPython/Sliver1/question9465.py
@@ -0,0 +1,30 @@
+import sys
+
+input = sys.stdin.readline
+
+T = int(input())
+
+for _ in range(T):
+    n = int(input())
+    arr = [list(map(int,input().split())) for _ in range(2)]
+    
+    DP = [[0] * n for _ in range(2)]
+    
+    # 길이가 1일 경우
+    DP[0][0] = arr[0][0]
+    DP[1][0] = arr[1][0]
+    if n == 1:
+        print(max(DP[0][0],DP[1][0]))
+        continue
+    
+    # 길이가 2일 경우
+    DP[0][1] = arr[1][0] + arr[0][1]
+    DP[1][1] = arr[0][0] + arr[1][1]
+    if n == 2:
+        print(max(DP[0][1],DP[1][1]))
+        continue
+    
+    for i in range(2, n):
+        DP[0][i] = max(DP[1][i-2],DP[1][i-1]) + arr[0][i] 
+        DP[1][i] = max(DP[0][i-2],DP[0][i-1]) + arr[1][i]
+    print(max(DP[0][i], DP[1][i]))
